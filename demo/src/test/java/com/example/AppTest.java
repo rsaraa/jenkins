918092635e49dae7c9e274eba6a5acbb26ff3923
neojarvis <techadmin@iamneo.ai>
2025-07-11 05:10:01 +0000

    message

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -16,6 +16,6 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-        System.out.println("Hello from ")
+        System.out.println("Hello Jenkins!!")
     }
 }
